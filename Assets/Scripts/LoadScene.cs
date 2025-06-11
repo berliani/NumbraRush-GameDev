9823d2d5b95eff498e295a657facbf8c26d1a5bf
berliani risqi <berlianirisqi1712@gmail.com>
2025-06-11 22:41:42 +0700

    addExitAboutButton

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -1,13 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
-    public void ChangeScene(string sceneName){
+    public GameObject AboutPanel;
 
-    SceneManager.LoadScene(sceneName);
-    
+    // Ganti scene
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Keluar game
+    public void ExitGame()
+    {
+        Debug.Log("Keluar dari game...");
+        Application.Quit(); // hanya bekerja saat di build
+    }
+
+    // Tampilkan About
+    public void ShowAbout()
+    {
+        AboutPanel.SetActive(true);
+    }
+
+    // Tutup About
+    public void HideAbout()
+    {
+        AboutPanel.SetActive(false);
     }
 }
